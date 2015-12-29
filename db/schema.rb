823a6da5f800f47766cbd6a5314eadb0c09ba867
qQZKDOHZc8Gdvkif386y <31friaKxpHmyWpvRcZob@gmail.com>
2015-12-29 02:36:53 -0500

    asdf

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151229070827) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "responses", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
